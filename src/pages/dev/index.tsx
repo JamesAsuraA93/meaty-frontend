3b499b6da629a8a52d818d8a9588d909d030150d
DOmENos <121551103+DOmENos@users.noreply.github.com>
2024-04-26 21:48:15 +0700

    Add files via upload

---
+++ src/pages/dev/index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@/components/typography";
+import React from "react";
+import ProductCard from "@/components/common/ProductCard";
+import CarouselCard from "@/components/common/CarouselCard";
+
+export default function DevHome() {
+  return (
+    <div>
+      <ProductCard/>
+      <CarouselCard/>
+    </div>
+  );
+}
