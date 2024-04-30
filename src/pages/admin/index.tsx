26a3fdb62950f4ea032884630e8166708f8861ad
JJSenior03 <sathit.jun03@gmail.com>
2024-04-30 16:29:04 +0700

    implement api order

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -18,12 +18,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Product {
   id: string;
   name: string;
   price: number;
+  filePath?: string;
 }
 
 interface OrderItem {
@@ -47,183 +49,42 @@ interface Order {
   items: OrderItem[];
 }
 
-const initialOrders = [
-  {
-    id: "1",
-    user_id: "1",
-    total_price_amount: 120.0,
-    discount: 10.0,
-    delivery_fee: 5.0,
-    status: "PENDING",
-    created_at: "2021-09-15T12:34:56Z",
-    items: [
-      {
-        id: "101",
-        order_id: "1",
-        product_id: "001",
-        quantity: 2,
-        subtotal: 60.0,
-        product: { id: "001", name: "Product A", price: 30.0 },
-      },
-      {
-        id: "102",
-        order_id: "1",
-        product_id: "002",
-        quantity: 2,
-        subtotal: 60.0,
-        product: { id: "002", name: "Product B", price: 30.0 },
-      },
-      {
-        id: "103",
-        order_id: "1",
-        product_id: "003",
-        quantity: 2,
-        subtotal: 60.0,
-        product: { id: "003", name: "Product C", price: 30.0 },
-      },
-    ],
-  },
-  {
-    id: "2",
-    user_id: "2",
-    total_price_amount: 200.0,
-    discount: 20.0,
-    delivery_fee: 10.0,
-    status: "SHIPPING",
-    created_at: "2021-10-01T11:22:33Z",
-    items: [
-      {
-        id: "104",
-        order_id: "2",
-        product_id: "004",
-        quantity: 1,
-        subtotal: 100.0,
-        product: { id: "004", name: "Product D", price: 100.0 },
-      },
-      {
-        id: "105",
-        order_id: "2",
-        product_id: "005",
-        quantity: 2,
-        subtotal: 100.0,
-        product: { id: "005", name: "Product E", price: 50.0 },
-      },
-    ],
-  },
-  {
-    id: "3",
-    user_id: "3",
-    total_price_amount: 300.0,
-    discount: 30.0,
-    delivery_fee: 15.0,
-    status: "DELIVERED",
-    created_at: "2021-11-12T14:45:00Z",
-    items: [
-      {
-        id: "106",
-        order_id: "3",
-        product_id: "006",
-        quantity: 3,
-        subtotal: 300.0,
-        product: { id: "006", name: "Product F", price: 100.0 },
-      },
-    ],
-  },
-  {
-    id: "4",
-    user_id: "4",
-    total_price_amount: 400.0,
-    discount: 50.0,
-    delivery_fee: 20.0,
-    status: "CANCELED",
-    created_at: "2021-12-24T17:00:00Z",
-    items: [
-      {
-        id: "107",
-        order_id: "4",
-        product_id: "007",
-        quantity: 4,
-        subtotal: 400.0,
-        product: { id: "007", name: "Product G", price: 100.0 },
-      },
-    ],
-  },
-  {
-    id: "5",
-    user_id: "5",
-    total_price_amount: 500.0,
-    discount: 25.0,
-    delivery_fee: 25.0,
-    status: "PENDING",
-    created_at: "2022-01-05T19:21:00Z",
-    items: [
-      {
-        id: "108",
-        order_id: "5",
-        product_id: "008",
-        quantity: 5,
-        subtotal: 500.0,
-        product: { id: "008", name: "Product H", price: 100.0 },
-      },
-    ],
-  },
-  {
-    id: "6",
-    user_id: "6",
-    total_price_amount: 600.0,
-    discount: 60.0,
-    delivery_fee: 30.0,
-    status: "SHIPPING",
-    created_at: "2022-02-14T10:30:00Z",
-    items: [
-      {
-        id: "109",
-        order_id: "6",
-        product_id: "009",
-        quantity: 6,
-        subtotal: 600.0,
-        product: { id: "009", name: "Product I", price: 100.0 },
-      },
-    ],
-  },
-];
-
-function StatusSelect({
-  value,
-  onChange,
-}: {
-  value: string;
-  onChange: (value: string) => void;
-}) {
-  return (
-    <Select>
-      <SelectTrigger className="w-full">
-        <SelectValue placeholder={value || "Select status"} />
-      </SelectTrigger>
-      <SelectContent className="z-[999]">
-        <SelectGroup>
-          <SelectLabel>Status</SelectLabel>
-          <SelectItem value="PENDING" onSelect={() => onChange("PENDING")}>
-            Pending
-          </SelectItem>
-          <SelectItem value="SHIPPING" onSelect={() => onChange("SHIPPING")}>
-            Shipping
-          </SelectItem>
-          <SelectItem value="DELIVERED" onSelect={() => onChange("DELIVERED")}>
-            Delivered
-          </SelectItem>
-          <SelectItem value="CANCELED" onSelect={() => onChange("CANCELED")}>
-            Canceled
-          </SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  );
-}
+// function StatusSelect({
+//   value,
+//   onChange,
+// }: {
+//   value: string;
+//   onChange: (value: string) => void;
+// }) {
+//   return (
+//     <Select>
+//       <SelectTrigger className="w-full">
+//         <SelectValue placeholder={value || "Select status"} />
+//       </SelectTrigger>
+//       <SelectContent className="z-[999]">
+//         <SelectGroup>
+//           <SelectLabel>Status</SelectLabel>
+//           <SelectItem value="PENDING" onSelect={() => onChange("PENDING")}>
+//             Pending
+//           </SelectItem>
+//           <SelectItem value="SHIPPING" onSelect={() => onChange("SHIPPING")}>
+//             Shipping
+//           </SelectItem>
+//           <SelectItem value="DELIVERED" onSelect={() => onChange("DELIVERED")}>
+//             Delivered
+//           </SelectItem>
+//           <SelectItem value="CANCELED" onSelect={() => onChange("CANCELED")}>
+//             Canceled
+//           </SelectItem>
+//         </SelectGroup>
+//       </SelectContent>
+//     </Select>
+//   );
+// }
 
 export default function AdminOrders() {
   // const router = useRouter();
-  const [orders, setOrders] = useState<Order[]>(initialOrders);
+  const [orders, setOrders] = useState<Order[]>([]);
   const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
   const [newStatus, setNewStatus] = useState<string>("");
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -237,12 +98,33 @@ export default function AdminOrders() {
   );
 
   useEffect(() => {
-    setOrders((orders) =>
-      orders.map((order) => ({
-        ...order,
-        formattedDate: new Date(order.created_at).toLocaleDateString(),
-      })),
-    );
+    axios.get('http://localhost:8003/orders')
+      .then(response => {
+        const fetchedOrders = response.data.map((order: { id: { toString: () => any; }; items: any[]; status: any; }) => ({
+          id: order.id.toString(),
+          user_id: order.items[0]?.order?.user?.email || 'Unknown', 
+          total_price_amount: order.items.reduce((acc: number, item: { quantity: number; product: { price: number; }; }) => acc + item.quantity * item.product.price, 0),
+          discount: 0,
+          delivery_fee: 0,
+          status: order.status,
+          created_at: new Date().toISOString(), 
+          items: order.items.map((item: { product: { name: any; price: number; filePath: any; }; quantity: number; }) => ({
+            id: `${order.id}-${item.product.name}`,
+            order_id: order.id.toString(),
+            product_id: 'Unknown',
+            quantity: item.quantity,
+            subtotal: item.quantity * item.product.price,
+            product: {
+              id: 'Unknown',
+              name: item.product.name,
+              price: item.product.price,
+              filePath: item.product.filePath
+            }
+          }))
+        }));
+        setOrders(fetchedOrders);
+      })
+      .catch(error => console.error('Failed to fetch orders:', error));
   }, []);
 
   const openModal = (order: Order) => {
@@ -255,20 +137,23 @@ export default function AdminOrders() {
     setIsModalOpen(false);
   };
 
-  const handleStatusChange = (status: string) => {
-    setNewStatus(status);
+  const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setNewStatus(event.target.value);
   };
 
   const saveStatusChange = () => {
     if (selectedOrder) {
-      setOrders((orders) =>
-        orders.map((order) =>
-          order.id === selectedOrder.id
-            ? { ...order, status: newStatus }
-            : order,
-        ),
-      );
-      closeModal();
+      const url = `http://localhost:8003/orders/${selectedOrder.id}/status`;
+      axios.put(url, { status: newStatus })
+        .then(() => {
+          setOrders((orders) =>
+            orders.map((order) =>
+              order.id === selectedOrder.id ? { ...order, status: newStatus } : order,
+            ),
+          );
+          closeModal();
+        })
+        .catch(error => console.error('Failed to update order status:', error));
     }
   };
 
@@ -360,10 +245,15 @@ export default function AdminOrders() {
                   <p>No items found for this order.</p>
                 )}
               </div>
-              <StatusSelect
-                value={selectedOrder.status}
-                onChange={handleStatusChange}
-              />
+              <div>
+                <label>Status: </label>
+                <select value={newStatus} onChange={handleStatusChange} className="border rounded p-2">
+                  <option value="PENDING">Pending</option>
+                  <option value="SHIPPING">Shipping</option>
+                  <option value="DELIVERED">Delivered</option>
+                  <option value="CANCELED">Canceled</option>
+                </select>
+              </div>
               <Button onClick={saveStatusChange} className="mr-4 mt-4">
                 Save Changes
               </Button>
